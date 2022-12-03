7eb9593de8ca1b85c92a92adee682f2ab69fbbcf
hisoka <1399952343@qq.com>
2022-12-03 22:54:02 +0800

    update: 重构router

---
+++ src/router/config/routes.ts
@@ -0,0 +1,11 @@
+import { Bar } from "../../views/Bar";
+import { Foo } from "../../views/Foo";
+
+export const routes = [
+    {
+        path: '/foo', component: Foo
+    },
+    {
+        path: '/bar', component: Bar
+    }
+]
---
+++ src/router/router.ts
@@ -0,0 +1,5 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { history } from './config/history'
+import { routes } from './config/routes'
+export const router = createRouter({ history, routes })
+
