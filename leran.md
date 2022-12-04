67e17ac1f9448e417d4266218ca7554b988f4866
hisoka <1399952343@qq.com>
2022-12-04 16:19:55 +0800

    update: 暂时使用png

--- leran.md
+++ leran.md
@@ -2,3 +2,5 @@ template下ref不加.value 因为template会自动给你加
 tsx下return div ref值要加.value
 
 css modules   scss   安装sass  1.19.11
+
+问题：svg不展示 暂时换成png
--- src/views/welcome.tsx
+++ src/views/welcome.tsx
@@ -1,10 +1,18 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
-
+import s from './welcome.module.scss';
+import logo from '../assets/icons/logo.png'
 export const Welcome = defineComponent({
     setup() {
         return () => (
-            <div>
+            <div class={s.wrapper}>
+                <div class={s.skip}>
+                    <span>跳过</span>
+                </div>
+                <header>
+                    <img src={logo} />
+                    <div>山竹记账</div>
+                </header>
                 <RouterView />
             </div>
         )
