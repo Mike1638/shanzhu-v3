bf4ed24dcc5207b21cb1bfe7413e171db6bd0e37
hisoka <1399952343@qq.com>
2022-12-04 22:31:58 +0800

    update: welcome first

--- src/components/welcome/first.tsx
+++ src/components/welcome/first.tsx
@@ -1,7 +1,19 @@
 import { defineComponent } from "vue";
+import { RouterLink } from "vue-router";
+import logo from '../../assets/icons/saving-pot.png'
+import s from '../../views/welcome.module.scss'
 
 export const First = defineComponent({
     setup() {
-        return () => <div>first</div>
+        return () => (
+            <div class={s.card}>
+                <div class={s.content}>
+                    <img src={logo} />
+                    <div class={s.money}>会挣钱</div>
+                    <div>还要会省钱</div>
+                    <router-link class={s.nextpage} to='/welcome/second'>下一页</router-link>
+                </div>
+            </div>
+        )
     }
 })
